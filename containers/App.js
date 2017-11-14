c26cc17e0ef998aa799e03e51116ff4135fe7e97
Rui Malheiro <rmpmalheiro@gmail.com>
2017-11-14 20:29:03 +0000

    Initial commit

---
+++ containers/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  constructor(props) {
+    super();
+    console.log('LOL');
+  }
+
+  render() {
+    return <p>This is my new react app</p>;
+  }
+}
