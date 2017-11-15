debc5753cd3f7423fb56f9466e133186edcba125
Rui Malheiro <rmpmalheiro@gmail.com>
2017-11-15 21:15:44 +0000

    Adding initial css; Button control

--- containers/App.js
+++ containers/App.js
@@ -3,10 +3,39 @@ import React, { Component } from 'react';
 export default class App extends Component {
   constructor(props) {
     super();
-    console.log('LOL');
+
+    this.state = {
+      mainDivClass: 'mainDivIncorrect',
+      wrapperClass: 'wrapper',
+      div1Class: 'text1',
+      div2Class: 'text2',
+      buttonClass: null
+    };
+  }
+
+  test(e) {
+    e.preventDefault();
+
+    if (this.state.buttonClass) {
+      this.setState({ buttonClass: null });
+    } else {
+      this.setState({ buttonClass: 'move' });
+    }
   }
 
   render() {
-    return <p>This is my new react app</p>;
+    return (
+      <div className={this.state.mainDivClass}>
+        <div className={this.state.wrapperClass}>
+          <button
+            id="button"
+            className={this.state.buttonClass}
+            onClick={this.test.bind(this)}
+          />
+          <div className={this.state.div1Class}>text1</div>
+          <div className={this.state.div2Class}>text2</div>
+        </div>
+      </div>
+    );
   }
 }
